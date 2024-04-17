c40daa581b61e475598adc9672db692d913b5db5
Amer Tricic <amer.tricic@live.com>
2024-04-17 15:20:22 +0200

    feat: update companySchema

--- src/models/company.js
+++ src/models/company.js
@@ -5,8 +5,21 @@ const dbPath = dbConfig.path;
 const { Schema } = new dbLocal({ path: dbPath });
 
 const companySchema = {
-  _id: Number,
-  name: String,
+  _id: { type: Number, required: true },
+  companyId: { type: Number, required: true },
+  companyTaxId: { type: Number, required: false },
+  companyFullName: { type: String, required: true },
+  companyShortName: { type: String, required: true },
+  companyType: { type: String, required: true },
+  companyOffice: {
+    city: { type: String, required: true },
+    street: { type: String, required: true },
+    streetNumber: { type: String, required: true },
+  },
+  companyContact: {
+    phone: { type: String, required: false },
+    email: { type: String, required: false },
+  },
 };
 
 const Company = Schema("Company", companySchema);
