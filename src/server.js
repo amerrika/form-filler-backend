e7360ec4cf4ff48e4a88900990049fc9d9a681be
Amer Tricic <amer.tricic@live.com>
2024-05-16 19:55:11 +0200

    feat: add cors, update localhost,

--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const companyRoutes = require("./routes/companyRoutes")
+const cors = require("cors");
+const companyRoutes = require("./routes/companyRoutes");
 
 // Express app
 const app = express();
 
-// Config variables
-const localHost = "127.0.0.1";
+// Configs
+const localHost = "localhost";
 const PORT = 3030;
 
 // Middleware
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Working");
 });
-app.use("/api/companies", companyRoutes)
+
+app.use("/api/companies", companyRoutes);
 
 // Listen
 app.listen(PORT, localHost, () => {
